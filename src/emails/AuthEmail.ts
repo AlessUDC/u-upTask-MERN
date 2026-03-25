6a4a198070e71e5ef35819a85841ffd255b941ba
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-03-25 00:10:01 -0500

    Implementar clase para el envío de correos de confirmación

---
+++ src/emails/AuthEmail.ts
@@ -0,0 +1,25 @@
+import { transporter } from "../config/nodemailer"
+
+interface IEmail {
+    email: string
+    name: string
+    token: string
+}
+
+export class AuthEmail {
+    static sendConfirmationEmail = async (user: IEmail) => {
+        const info = await transporter.sendMail({
+            from: 'Uptask <admin@uptask.com>',
+            to: user.email,
+            subject: 'Uptask - Confirma tu cuenta',
+            text: `UpTask - Confirma tu cuenta en Uptask`,
+            html: `<p>Hola ${user.name}, tu cuenta ha sido creada exitosamente.</p>
+            <p>Visita el siguiente enlace para confirmar tu cuenta:</p>
+            <a href="">Confirma tu cuenta</a>
+            <p>Ingresa el código <b>${user.token}</b></p>
+            <p>Este código expirará en 10 minutos</p>
+            <p>Si no creaste esta cuenta, ignora este correo.</p>`
+        })
+        console.log('Email enviado', info.messageId)
+    }
+}
